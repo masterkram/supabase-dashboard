7ac5412687bc4b07f062eb994a882cb04b7cf0d0
Mark Bruderer <markbruderer123@gmail.com>
2024-01-22 13:44:51 +0100

    initial commit.

---
+++ components/projects/empty.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button type="button" @click="isOpen = true"
+      class="relative block w-full rounded-lg border-2 border-dashed border-zinc-300 p-2 text-center hover:border-zinc-400 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2">
+      <Icon class="w-8 h-8 text-emerald-500" name="carbon:lightning" />
+      <span class="mt-2 block text-sm font-semibold text-gray-900">Connect your supabase project.</span>
+    </button>
+    <USlideover v-model="isOpen">
+      <div class="p-4 flex-1">
+        <Placeholder class="h-full" />
+      </div>
+    </USlideover>
+  </div>
+</template>
+
+
+<script setup>
+const isOpen = ref(false)
+</script>
---
+++ pages/projects.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>Your projects:</h2>
+    <div class="border-b border-zinc-200 my-4"></div>
+    <span class="font-medium text-zinc-600">No Projects Yet.</span>
+    <div class="border-b border-zinc-200 my-4"></div>
+    <ProjectsEmpty />
+  </div>
+</template>
